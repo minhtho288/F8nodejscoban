44a49c696f761e2e899ae2761e9345ca09556f55
minhtho288 <minhtho288>
2021-04-21 22:53:57 +0700

    POST method

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ const path = require('path');
 const express = require('express');
 // const morgan = require('morgan');
 const handlebars  = require('express-handlebars');
+const { urlencoded } = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname,'public')));
+app.use(urlencoded({
+  extended:true
+}));
+app.use(express.json());
 //HTTP logger
 // app.use(morgan('combined'));
 
@@ -26,6 +31,10 @@ app.get('/news', (req, res) => {
 app.get('/search', (req, res) => {
   // console.log(req.query.q);
   res.render('search');
+});
+app.post('/search', (req, res) => {
+  // console.log(req.body);
+  res.send('');
 });
 
 app.listen(port, () => {
