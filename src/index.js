6f2e505742c53db112b15012746444a21e11338a
minhtho288 <minhtho288>
2021-04-23 23:15:40 +0700

    prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 const path = require('path');
 const express = require('express');
 // const morgan = require('morgan');
-const handlebars  = require('express-handlebars');
+const handlebars = require('express-handlebars');
 const { urlencoded } = require('express');
 const app = express();
 const port = 3000;
 
 const route = require('./routes');
-app.use(express.static(path.join(__dirname,'public')));
-app.use(urlencoded({
-  extended:true
-}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 //HTTP logger
 // app.use(morgan('combined'));
 
 //templace engine
-app.engine('hbs', handlebars({
-  extname: '.hbs'
-})); 
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 // console.log('PATH: ', path.join(__dirname, 'resources/views'));
@@ -26,5 +31,5 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
