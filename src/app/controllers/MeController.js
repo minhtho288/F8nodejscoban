dddf49ffb62d9c630e08281b84e01c6dc7cb7ac4
minhtho288 <minhtho288>
2021-05-01 23:38:48 +0700

    Select all  with checkbox

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,13 +4,31 @@ const { mutipleMongooseToObject } = require('../../util/mongoose');
 class MeController {
     //[GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then(courses => res.render('me/stored-courses',{
-                courses: mutipleMongooseToObject(courses)
-            }))
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses,deletedCount])=>
+                res.render('me/stored-courses',{
+                    deletedCount,
+                    courses: mutipleMongooseToObject(courses)
+                })
+        ) 
             .catch(next);
-        
     }
+
+//lấy ra số lượng đã xóa
+        // Course.countDocumentsDeleted()
+        //     .then((deletedCount) => {
+        //         console.log(deletedCount);
+        //     })
+        //     .catch(() => {
+                
+        //     });
+//lấy dữ liệu ra
+    //     Course.find({})
+    //         .then(courses => res.render('me/stored-courses',{
+    //             courses: mutipleMongooseToObject(courses)
+    //         }))
+    //         .catch(next);
+    // }
     //[GET] /me/trash/courses
     trashCourses(req, res, next) {
         Course.findDeleted({})
